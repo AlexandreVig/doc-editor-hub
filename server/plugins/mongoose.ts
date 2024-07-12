c188c0f407b529fe6dc0d85bfa8f9e7c11147191
AlexandreVig <alexandre.vigneau@epitech.eu>
2024-07-12 22:42:05 +0200

    [FIX] MongoDB authentification

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -1,12 +1,20 @@
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default defineNitroPlugin(async (nitroapp) => {
   try {
-    const mongoDb= process.env.MONGODB_URL ?? "mongodb://localhost:27017/doc-editor"
-    await mongoose.connect(mongoDb);
-    console.log('Connected to MongoDB');
+    const mongoDb =
+      process.env.MONGODB_URL ?? "mongodb://localhost:27017/doc-editor";
+    if (process.env.MONGODB_USER && process.env.MONGODB_PASSWD) {
+      const authParams = {
+        user: process.env.MONGODB_USER,
+        pass: process.env.MONGODB_PASSWD,
+      };
+      await mongoose.connect(mongoDb, authParams);
+    } else {
+      await mongoose.connect(mongoDb);
+    }
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 });
